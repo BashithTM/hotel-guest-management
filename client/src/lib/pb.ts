557ad142ef43eb4ec185c915b48eecbbc13cdd60
BashithTM <mohammedbashith24@gmail.com>
2025-08-29 06:26:38 +0530

    pb

--- client/src/lib/pb.ts
+++ client/src/lib/pb.ts
@@ -2,5 +2,3 @@
 import PocketBase from "pocketbase";
 
 export const pb = new PocketBase(import.meta.env.VITE_PB_URL || "http://127.0.0.1:8090");
-// If you use auth, restore from localStorage:
-// pb.authStore.loadFromCookie(document.cookie)
